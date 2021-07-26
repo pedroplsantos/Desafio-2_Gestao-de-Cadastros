7a46cf5cce7f51ad3ae472400bcafbfb91d23025
Pedro Lima <pplsantos89@gmail.com>
2021-07-25 23:46:06 -0300

    ainda falta terminar

--- e-commerce/client/src/pages/CategoryPage.js
+++ e-commerce/client/src/pages/CategoryPage.js
@@ -2,6 +2,23 @@ import React from 'react'
 import { useParams } from "react-router-dom"
 import { Container } from 'react-bootstrap'
 import CardDisplay from '../components/CardDisplay/CardDisplay'
+import 
+
+const CATEGORY_QUERY = gql`
+{
+    category(slug: "mammals"){
+        id
+        category
+        slug
+        animals{
+            id
+            title
+            price
+            image
+        }
+    }
+}
+`
 
 function CategoryPage() {
     
